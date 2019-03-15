8a0dc1220f2127f75af86082011631fb0de9da12
Paulo Coelho <paulo.coelho1989@gmail.com>
2019-03-15 15:00:40 +0000

    Adds Client to consume the WCF Services

---
+++ WCF_REST_Service/WCF_Service_Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Service_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxyServer = new WebClient();
+            var serviceUrl = String.Format("http://localhost:50525/Services/PatientService.svc/AllPatients");
+            var _data = proxyServer.DownloadData(serviceUrl);
+            var _memoryStream = new MemoryStream(_data);
+            var reader = new StreamReader(_memoryStream);
+            var result = reader.ReadToEnd();
+
+
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
