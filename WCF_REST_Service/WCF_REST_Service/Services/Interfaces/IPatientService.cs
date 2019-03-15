f7d410aafb23fa17313676dd887415cec196dd55
Paulo Coelho <paulo.coelho1989@gmail.com>
2019-03-15 14:02:49 +0000

    First commit

---
+++ WCF_REST_Service/WCF_REST_Service/Services/Interfaces/IPatientService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WCF_REST_Service.DataAccess.EF;
+
+namespace WCF_REST_Service.Services.Interfaces
+{
+    [ServiceContract]
+    public interface IPatientService
+    {
+        [OperationContract]
+        [WebGet]
+        List<Patient> GetAllPatients();
+
+        [OperationContract]
+        [WebGet]
+        Patient GetPatient(int id);
+    }
+}
---
+++ WCF_REST_Service/WCF_REST_Service/Services/PatientService.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WCF_REST_Service.DataAccess.EF;
+using WCF_REST_Service.Services.Interfaces;
+
+namespace WCF_REST_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PatientService : IPatientService
+    {
+        public List<Patient> GetAllPatients()
+        {
+            using (var db = new PatientsDBEntities())
+            {
+                return db.Patients.ToList();
+            }
+        }
+
+        public Patient GetPatient(int id)
+        {
+            using (var db = new PatientsDBEntities())
+            {
+                return db.Patients.Find(id);
+            }
+        }
+    }
+}
